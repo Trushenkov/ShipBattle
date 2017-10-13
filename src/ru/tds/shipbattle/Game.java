e1986cb67b215e1c15ea9b07b8bdf91676948db6
Трушенков Дмитрий <diman28051999@gmail.com>
2017-10-13 13:13:34 +0300

    Изменены и добавлены новые комментарии к методам. А также выолнены некоторые исправления в методах.

--- src/ru/tds/shipbattle/Game.java
+++ src/ru/tds/shipbattle/Game.java
@@ -9,27 +9,45 @@ import java.util.Scanner;
  * @author Trushenkov Dmitry 15ИТ18
  */
 public class Game {
+
     public static void main(String[] args) {
         ArrayList<String> localOfShip = new ArrayList<>();
         initShipLocation(localOfShip);
         Ship ship = new Ship();
         ship.setLocation(localOfShip);
+        System.out.println(ship);
         countOfTry(ship);
     }
 
+    /**
+     * Метод для заполнения ArrayLista с местоположением корабля.
+     *
+     * @param arrayList пустой ArrayList
+     */
     public static void initShipLocation(ArrayList<String> arrayList) {
         arrayList.add(String.valueOf((int) ((Math.random() * 7))));
         arrayList.add(String.valueOf((Integer.parseInt(arrayList.get(0))) + 1));
         arrayList.add(String.valueOf(Integer.parseInt(arrayList.get(1)) + 1));
     }
 
+    /**
+     * Метод для ввода пользователем координат выстрела.
+     *
+     * @return координата выстрела, введенная пользователем.
+     */
     public static String input() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите координаты выстрела от 0 до 9: ");
-        int shot = sc.nextInt();
-        return String.valueOf(shot);
+        String shot = sc.next();
+        return shot;
     }
-    public static void countOfTry(Ship ship){
+
+    /**
+     * Метод для подсчета количества попыток, понадобившихся пользователю для того, чтобы потопить корабль.
+     *
+     * @param ship корабль, который нужно потопить.
+     */
+    public static void countOfTry(Ship ship) {
         int countOfTry = 0;
         while (!ship.getLocation().isEmpty()) {
             System.out.println(ship.check(input()));
